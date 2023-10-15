e96e893f1fb36c0aa65252631f4042bded79db22
Eliana Setiabudi <amidis100@gmail.com>
2023-10-15 22:02:10 +0800

    added Action and FSM scripts

---
+++ Lab 1 - Super Mario Bros/Assets/Scripts/FSM Scripts/SetupAnimatorAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "PluggableSM/Actions/SetupAnimator")]
+public class SetupAnimatorAction : Action
+{
+    public RuntimeAnimatorController animatorController;
+    public override void Act(StateController controller)
+    {
+        controller.gameObject.GetComponent<Animator>().runtimeAnimatorController = animatorController;
+    }
+}
